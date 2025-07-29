824623fac6ac04400a0a8e75a3401c23bb35e530
조윤지 <yunji8533@naver.com>
2025-07-29 15:59:07 +0900

    ✨: 라우터 설정

--- session1/src/App.jsx
+++ session1/src/App.jsx
@@ -1,15 +1,34 @@
-import "./App.scss";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Signup/Login";
 import Main from "./pages/main/Main";
+import NoticePage from "./pages/notice/NoticePage";
+import NoticeDetailPage from "./pages/notice/NoticeDetailPage";
+import MainLayout from "./component/MainLayout";
+import WorkResultPage from "./pages/notice/WorkResultPage";
+import PostMain from "./pages/post/PostMain";
+import PostWrite from "./pages/post/PostWrite";
+import PostDetail from "./pages/post/PostDetail";
 
 function App() {
   return (
-    <div className="App">
-      <Signup />
-      {/* <Login /> */}
-      {/* <Main /> */}
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route element={<MainLayout />}> // 중첩 라우터
+            <Route path="/main" element={<Main />} />
+            <Route path="/notice" element={<NoticePage />} />
+            <Route path="/notice/:id" element={<NoticeDetailPage />} />
+            <Route path="/workresult" element={<WorkResultPage />} />
+            <Route path="/post" element={<PostMain />} />
+            <Route path="/post/write" element={<PostWrite />} />
+            <Route path="/post/:id" element={<PostDetail />} />
+          </Route>
+        </Routes>
+       </div>
+    </BrowserRouter>
   );
 }
 
